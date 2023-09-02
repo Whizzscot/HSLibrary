7b6783ae0b202acd76c6c82f5b444fc9f47dfc75
Elliot <elliot.mcleish@gmail.com>
2023-09-02 15:27:24 +1200

    quick save

--- script.js
+++ script.js
@@ -5,7 +5,43 @@ function ev(el){
 	console.log('Hello', el.innerText);
 }
 
-function removeTag(tagElem, inputElem){
+function cancelEvent(e){e.preventDefault();e.stopPropagation();return false;}
+
+function closeAllDialogs(){
+	document.querySelectorAll('dialog:not(.self-close)').forEach(el=>el.close());
+}
+
+const TagContextDialog = document.querySelector("#tag-context-dialog");
+const TagContextMenu = TagContextDialog.firstElementChild;
+
+function tagContextEvent(tag, input){
+	if(TagContextDialog.open) return TagContextDialog.close();
+	if(matchMedia('(pointer:fine)').matches){
+		let elemBox = tag.getBoundingClientRect();
+		TagContextDialog.style.left = `min(${elemBox.right - window.scrollX}px, calc(100% - 5.2rem - var(--padding)))`;
+		TagContextDialog.style.bottom = `calc(${window.innerHeight - elemBox.y - window.scrollY}px + var(--padding))`;
+		TagContextDialog.show();
+	}else{
+		TagContextDialog.showModal();
+	}
+	TagContextDialog.returnValue = "none";
+	TagContextMenu.setAttribute("selected-tag", tag.getAttribute("tag-value"));
+	tag.classList.add("selected");
+	TagContextDialog.addEventListener("close", ()=>{
+		switch(TagContextDialog.returnValue){
+			case "edit":
+				editTag(tag, input);
+			break;
+			case "remove":
+				tag.remove();
+			break;
+		}
+		tag.classList.remove("selected");
+		TagContextMenu.removeAttribute("selected-tag");
+	}, {once:true});
+}
+
+function editTag(tagElem, inputElem){
 	inputElem = inputElem || tagElem.parentElement.parentElement.parentElement.querySelector('.tag-input');
 	if(!inputElem) return console.error('pointer to inputElem invalid');
 	inputElem.value = tagElem.innerText;
@@ -27,27 +63,40 @@ function encodeTag(tag){
 	return encodeURIComponent(tag);
 }
 
-function applyTag(inputElem){
-	inputElem.focus();
+function getTagById(parent=new HTMLElement(), id="", isValue=false){
+	return parent.querySelector(`.applied-tag[tag-${isValue ? "value" : "id"}='${id.replace(/'/g, "\\'")}']`);
+}
+
+function applyTag(inputElem, focus=true){
+	if(focus) inputElem.focus();
 	if(!inputElem.value) return;
 	let tag = inputElem.value;
 	let encodedTag = encodeTag(tag);
 	let list = document.querySelector(`form.item-info:has(#${inputElem.id}) .applied-tags-list`);
-	if(list.querySelector(`.applied-tag[tag-id='${encodedTag.replace(/'/g,"\\'")}']`)) return;
+	if(getTagById(list, encodedTag)) return;
 	let orderId = inputElem.getAttribute('tag-order-id') || 0;
 	orderId = Number(orderId) || 0;
 	let tagElem = document.createElement('span');
 	tagElem.classList.add('applied-tag');
 	tagElem.setAttribute('applied-order', orderId);
 	tagElem.setAttribute('tag-id', encodedTag);
-	tagElem.innerText = inputElem.value;
-	tagElem.addEventListener('contextmenu', e => removeTag(e.target, inputElem));
+	tagElem.setAttribute('tag-value', tag);
+	tagElem.innerText = tag;
+	tagElem.addEventListener('contextmenu', e => {e.preventDefault();tagContextEvent(e.target, inputElem)});
+	tagElem.addEventListener('mousedown', e => {if(matchMedia('(pointer:coarse)').matches){e.stopPropagation();tagContextEvent(e.target, inputElem)}});
 	inputElem.setAttribute('tag-order-id', ++orderId);
 	inputElem.value = '';
 	list.appendChild(tagElem);
 	let sortMode = document.querySelector(`form.item-info:has(#${inputElem.id}) .applied-tags-sort-select`).value;
 	if(sortMode != 'applied') sortTags(list, sortMode);
 
+}
+
+
+const tagInput = document.querySelector(".tag-input");
+for(let x = 0; x < 100; x++){
+	tagInput.value = Math.random().toString(36).slice(10);
+	applyTag(tagInput);
 }
 
 var tagCompareFunctions = {
